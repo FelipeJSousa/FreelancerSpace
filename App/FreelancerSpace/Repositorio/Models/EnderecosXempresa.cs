e484351a6dce727e7f718a188c88ed15b7f44d0e
Felipe Sousa <csfelipe8@gmail.com>
2021-03-31 21:00:48 -0300

    Added dependencies and scafolded models

---
+++ App/FreelancerSpace/Repositorio/Models/EnderecosXempresa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Repositorio.Models
+{
+    public partial class EnderecosXempresa
+    {
+        public int IdEndereco { get; set; }
+        public int IdEmpresa { get; set; }
+
+        public virtual Empresa IdEmpresaNavigation { get; set; }
+        public virtual Endereco IdEnderecoNavigation { get; set; }
+    }
+}
