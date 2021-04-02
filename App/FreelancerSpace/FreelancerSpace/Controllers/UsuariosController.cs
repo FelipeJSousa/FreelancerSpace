bd508cbe812d90075708afa6a4e85e54904dc916
Felipe Sousa <csfelipe8@gmail.com>
2021-04-01 22:16:23 -0300

    Projeto configurado

---
+++ App/FreelancerSpace/FreelancerSpace/Controllers/UsuariosController.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using FreelancerSpace.Models;
+using Microsoft.AspNetCore.Mvc;
+using Repositorio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Repositorio.Repositorios;
+
+namespace FreelancerSpace.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult salvar(UsuariosModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+
+                    var mapper = new Mapper(AutoMapperConfig.RegisterMappings());
+
+                    Usuario user = mapper.Map<Usuario>(model);
+
+                    UsuarioRepository rep = new UsuarioRepository();
+                    rep.add(user);
+
+                    ViewBag.message = "Usuário Salvo com Sucesso!";
+                }
+            }
+            catch (Exception)
+            {
+                ViewBag.message = "Não foi possível salvar o usuário!";
+            }
+
+            return View("Index");
+        }
+    }
+}
