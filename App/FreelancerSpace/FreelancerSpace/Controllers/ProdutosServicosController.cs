8b07a6810ab9d7b2ffe3305661469f96ac9477f1
Felipe Sousa <csfelipe8@gmail.com>
2021-04-06 22:32:27 -0300

    Realizado relacionamento entre models
    Adicionado dropdown de Ramo de atividade no produtosServicos

--- App/FreelancerSpace/FreelancerSpace/Controllers/ProdutosServicosController.cs
+++ App/FreelancerSpace/FreelancerSpace/Controllers/ProdutosServicosController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using FreelancerSpace.Models;
+using Microsoft.AspNetCore.Mvc;
+using Repositorio.Models;
+using Repositorio.Repositorios;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +12,25 @@ namespace FreelancerSpace.Controllers
 {
     public class ProdutosServicosController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Create()
         {
+            var mapper = new Mapper(AutoMapperConfig.RegisterMappings());
+            List<RamoAtividadeModel> listmodel = null;
+            try
+            {
+                var list = new RamoAtividadeRepository().getAll();
+                listmodel = mapper.Map<List<RamoAtividadeModel>>(list);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+
+            ViewBag.listRamoAtividade = listmodel;
+
             return View();
         }
     }
